e83529fc7cfdcb79fe27bb0becb2720621851224
Kleidis <kleidisn@hotmail.com>
2024-02-28 15:42:57 +0100

    Set wallpaper script

---
+++ setwallpaper.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if the script received exactly one argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <path_to_wallpaper>"
+    exit 1
+fi
+
+# The full path to the wallpaper is the first argument
+wallpaper_path="$1"
+
+# Extract the directory and wallpaper name from the path
+working_directory=$(dirname "$wallpaper_path")
+wallpaper_name=$(basename "$wallpaper_path")
+
+# Use swaybg with the -i flag to set the wallpaper
+swaybg -i "${wallpaper_path}" &
+
+# Use wal to set the colorscheme based on the wallpaper
+wal -i "${wallpaper_path}"
+
+# Copy the generated color scheme files to their respective locations
+cp -f "${HOME}"/.cache/wal/pywal.json "${HOME}"/.config/presets/user/pywal.json
+cp "${HOME}"/dotfiles/.cache/wal/pywal.kvconfig "${HOME}"/dotfiles/.config/Kvantum/pywal/pywal.kvconfig
+cp "${HOME}"/dotfiles/.cache/wal/pywal.svg "${HOME}"/.config/Kvantum/pywal/pywal.svg
+
+# Restart Waybar to apply the new color scheme
+~/dotfiles/restartwaybar.sh
+
+# Apply the pywal theme to GTK applications using gradience-cli
+gradience-cli apply -n pywal --gtk both
