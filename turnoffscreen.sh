a78f0d043ce04ef492edd7ee321ced112467bd88
kleidiss <kleidisn@hotmail.comm>
2024-03-02 12:22:33 +0100

    Switch to hyprlock from swaylock

--- turnoffscreen.sh
+++ turnoffscreen.sh
@@ -1,4 +1,4 @@
-swayidle -w timeout 300 'swaylock -f -c 000000' \
+swayidle -w timeout 300 'hyprlock -f -c 000000' \
             timeout 600 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on' \
-            before-sleep 'swaylock -f -c 000000' &
+            before-sleep 'hyprlock -f -c 000000' &
